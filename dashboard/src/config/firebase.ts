f4ffafca9e218d8d0940944c7bc93eff3812e4ac
Burhan Hamid Dar <burhanhamid912@gmail.com>
2026-05-31 11:45:32 +0530

    Update firebase.ts

--- dashboard/src/config/firebase.ts
+++ dashboard/src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 export const firebaseConfig = {
- apiKey: "xxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "xxxxxxxxxxxxxx.firebaseapp.com",
-  databaseURL: "https://xxxxxxxxxxxxxxxxxxxxxx.asia-southeast1.firebasedatabase.app",
-  projectId: "xxxxxxxxxxxxxxxxxxxx",
-  storageBucket: "xxxxxxxxxxxxxxxx.appspot.com",
-  messagingSenderId: "3847438xxxxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxxxxxx"
+ apiKey: "AIzaSyCX7fV4-Gt-4vFZAtSm6jwn6TWpbc3bBu4",
+  authDomain: "smart-irrigation-e2db4.firebaseapp.com",
+  databaseURL: "https://smart-irrigation-e2db4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-irrigation-e2db4",
+  storageBucket: "smart-irrigation-e2db4.appspot.com",
+  messagingSenderId: "642123031610",
+  appId: "1:642123031610:web:05d5142d48822e6bc64103"
 };
 
 // Initialize Firebase app, guard against duplicate initializations
